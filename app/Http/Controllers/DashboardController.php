9acf2ebca06211987c4398e9d265c2a3a372823b
punkestu <pangestubima89@gmail.com>
2025-04-15 08:53:44 +0700

    fix: change dashboard fallback

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,6 @@ namespace App\Http\Controllers;
 class DashboardController
 {
     public function route() {
-        return redirect()->route("dashboard.item");
+        return redirect()->route("dashboard.order");
     }
 }
